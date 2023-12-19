23fe89e97f03b950665f3b615a430214a5d19bbc
Amit Vishwakarma <52880868+AvTe@users.noreply.github.com>
2023-12-19 15:23:56 +0000

    datatypes summary

---
+++ 01_basic/Day-third.js
@@ -0,0 +1,34 @@
+// How memory work 
+
+// Primitive datatypes 
+// are in the 7 types 
+//  String, Number, Boolean, null, undefined, symbol, BigInt
+
+// Is js dynamic or not 
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// Refreence (Non primitive)
+// Array, Objects, Functions
+
+const heros = ["shakiman", "ironman", "superman"]
+let myobj = {
+    name: "Amit",
+    age: 22,
+}
+
+const myfunction = function() {
+    console.log("Hello world")
+}
+
+console.log(typeof anotherId);
